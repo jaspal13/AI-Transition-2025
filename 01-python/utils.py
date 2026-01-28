a76d040de3c7fc2f10f52cab355f61969e3cf03a
Jaspal Singh <jaspal.singh1303@gmail.com>
2026-01-28 14:34:15 +0530

    Add enrich age group function to util.py

--- 01-python/utils.py
+++ 01-python/utils.py
@@ -13,3 +13,17 @@ def age_group(age):
         return "adult"
     else:
         return "senior"
+
+def enrich_people_with_age_group(people):
+    """
+    Takes a list of people dicts and adds age_group to each.
+    Returns a NEW list.
+    """
+    return [
+        {
+            "name": p["name"],
+            "age": p["age"],
+            "age_group": age_group(p["age"])
+        }
+        for p in people
+    ]
